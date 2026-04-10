23750b6f03df202c545a8366ada6b8a42ae99edf
daniell escareno <daniellescareno7@gmail.com>
2026-04-10 06:49:58 -0700

    Set up multiple rows of multicolored bricks

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -8,7 +8,6 @@
 import SpriteKit
 import GameplayKit
 
-
 var ball = SKShapeNode()
 var paddle = SKSpriteNode()
 var bricks = [SKSpriteNode]()
@@ -66,19 +65,27 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     func didBegin(_ contact: SKPhysicsContact) {
         // ask each brick "Is It You?"
         for brick in bricks {
-        if contact.bodyA.node?.name == "brick" ||
-            contact.bodyB.node?.name == "brick" {
-            score += 1
-            updateLabels()
-            brick.removeFromParent()
-            removedBricks += 1
-            if removedBricks == bricks.count {
-                gameOver(winner: true)
+            if contact.bodyA.node == brick ||
+                contact.bodyB.node == brick {
+                score += 1
+                updateLabels()
+                if brick.color == .blue {
+                    brick.color = .orange
+                }
+                else if brick.color == .orange {
+                    brick.color = .green
+                }
+                else {
+                    brick.removeFromParent()
+                    removedBricks += 1
+                    if removedBricks == bricks.count {
+                        gameOver(winner: true)
+                    }
+                }
             }
         }
-        }
-        if contact.bodyA.node?.name == "loseZone" ||
-            contact.bodyB.node?.name == "loseZone" {
+        if contact.bodyA.node == loseZone ||
+            contact.bodyB.node == loseZone {
             lives -= 1
             if lives > 0 {
                 score = 0
@@ -183,10 +190,13 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         // now, figure the number and spacing of each row of bricks
         let count = Int(frame.width) / 55 // bricks per row
         let xOffset = (Int(frame.width) - (count * 55)) / 2 + Int(frame.minX) + 25
-        let y = Int(frame.maxY) - 65
-        for i in 0..<count {
-            let x = i * 55 + xOffset
-            makeBrick(x: x, y: y, color: .green)
+        let colors: [UIColor] = [.blue, .orange, .green]
+        for r in 0..<3 {
+            let y = Int(frame.maxY) - 65 - (r * 25)
+            for i in 0..<count {
+                let x = i * 55 + xOffset
+                makeBrick(x: x, y: y, color: colors[r])
+            }
         }
     }
     
